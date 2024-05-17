613cf2c62179c49a1064af4de321a218518728b3
bitterale <alemuri2308@gmail.com>
2024-05-17 12:59:09 +0300

    fresh

--- components/Hero.js
+++ components/Hero.js
@@ -13,7 +13,7 @@ const Hero = () => {
   className="relative bg-[url(https://images.pexels.com/photos/272980/pexels-photo-272980.jpeg?auto=compress&cs=tinysrgb&w=600)] bg-cover bg-center bg-no-repeat"
 >
   <div
-    className="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"
+    className="absolute inset-0 bg-white/25 sm:bg-transparent sm:from-white/25 sm:to-white/25 ltr:sm:bg-gradient-to-t "
   ></div>
 
   <div
@@ -23,24 +23,24 @@ const Hero = () => {
       <h1 className="text-3xl font-extrabold sm:text-5xl">
         Committed to 
 
-        <strong className="block font-extrabold text-blue-700"> Excellence </strong>
+        <strong className="block font-extrabold text-green-700"> Excellence </strong>
       </h1>
 
       <p className="mt-4 max-w-lg sm:text-xl/relaxed">
       To lay a strong foundation for future leaders
       </p>
 
-      <div className="mt-8 flex flex-wrap gap-4 text-center">
+      <div className="mt-8 flex  gap-4 text-center">
         <a
           href="#"
-          className="block w-full rounded  bg-green-600 px-12 py-3 text-sm font-medium text-white shadow  focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
+          className="block w-full rounded flex-1  bg-green-600 px-12 py-3 text-sm font-medium text-white shadow  focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
         >
           Apply Today
         </a>
 
         <a
           href="#"
-          className="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-green-600 shadow  focus:outline-none focus:ring active:text-rose-500 sm:w-auto"
+          className="block w-full rounded flex-1 bg-white px-12 py-3 text-sm font-medium text-green-600 shadow  focus:outline-none focus:ring active:text-rose-500 sm:w-auto"
         >
           About Us
         </a>
